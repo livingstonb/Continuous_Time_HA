bb598771e8ddd471eca895879d9fef59f0063d11
Brian Livingston <ak.brian09@gmail.com>
2019-09-05 09:17:51 -0500

    changes for server

--- master_con_effort.m
+++ master_con_effort.m
@@ -31,13 +31,13 @@ warning('off','MATLAB:nearlySingularMatrix')
 % SET OPTIONS
 % -------------------------------------------------------------------------
 
-runopts.Server      = 0; % sets param_index=slurm env var
-runopts.IterateRho  = 0;
-runopts.fast = 1; % use small grid for  debugging
+runopts.Server      = 1; % sets param_index=slurm env var
+runopts.IterateRho  = 1;
+runopts.fast = 0; % use small grid for  debugging
 runopts.ComputeMPCS = 1;
-runopts.ComputeMPCS_news = 1;
+runopts.ComputeMPCS_news = 0;
 runopts.SimulateMPCS = 1;
-runopts.SimulateMPCS_news = 1;
+runopts.SimulateMPCS_news = 0;
 
 runopts.params_file = 'get_params2';
 
