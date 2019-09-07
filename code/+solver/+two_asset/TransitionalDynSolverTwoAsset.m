4bd970aab1c4132e062878b578185eda6b0beb7f
Brian Livingston <ak.brian09@gmail.com>
2019-09-07 15:54:17 -0500

    bug fix in two asset mpcs out of news

--- code/+solver/+two_asset/TransitionalDynSolverTwoAsset.m
+++ code/+solver/+two_asset/TransitionalDynSolverTwoAsset.m
@@ -27,10 +27,10 @@ classdef TransitionalDynSolverTwoAsset < solver.TransitionalDynSolver
 		function deathin_cc_k = get_death_inflows(obj,cumcon_t_k,k)
             reshape_vec = [obj.p.nb_KFE*obj.p.na_KFE obj.p.nz obj.p.ny];
 			cumcon_t_z_k = reshape(cumcon_t_k,reshape_vec);
-            ytrans_cc_k = sum(obj.ytrans_offdiag(k,:) .* cumcon_t_k,2);
+            ytrans_cc_k = sum(obj.ytrans_offdiag(k,:) .* cumcon_t_k(:,k),2);
 
             if (obj.p.Bequests == 1) && (obj.p.ResetIncomeUponDeath == 1)
-                deathin_cc_k = obj.p.deathrate * sum(obj.income.ydist' .* cumcon_t_k,2);
+                deathin_cc_k = obj.p.deathrate * sum(obj.income.ydist' .* cumcon_t_k(:,k),2);
                 error('need to recode')
                 if nz > 1
                     error('not correctly coded for nz > 1')
@@ -38,13 +38,13 @@ classdef TransitionalDynSolverTwoAsset < solver.TransitionalDynSolver
             elseif (obj.p.Bequests == 1) && (obj.p.ResetIncomeUponDeath == 0)
                 deathin_cc_k = obj.p.deathrate * cumcon_t_k(:,k);
             elseif (obj.p.Bequests == 0) && (obj.p.ResetIncomeUponDeath == 1)
-                deathin_cc_k = obj.p.deathrate * sum(obj.income.ydist' .* cumcon_t_k(1,:),2);
+                deathin_cc_k = obj.p.deathrate * sum(obj.income.ydist' .* cumcon_t_k(1,k),2);
                 error('neet to recode')
                 if nz > 1
                     error('not correctly coded for nz > 1')
                 end
             elseif (obj.p.Bequests == 0) && (obj.p.ResetIncomeUponDeath == 0)
-                deathin_cc_k = obj.p.deathrate * squeeze(cumcon_t_z_k(obj.grids.loc0b,1,:,k));
+                deathin_cc_k = obj.p.deathrate * squeeze(cumcon_t_z_k(obj.grids.loc0b0a,:,k));
                 deathin_cc_k = kron(deathin_cc_k,ones(obj.p.nb_KFE*obj.p.na_KFE,1));
             end
         end
--- master_two_asset.m
+++ master_two_asset.m
@@ -45,7 +45,7 @@ runopts.DealWithSpecialCase = 0;
 
 % Select which parameterization to run from parameters file
 % (ignored when runops.Server = 1)
-runopts.param_index = 601;
+runopts.param_index = 1;
 
 runopts.serverdir = '/home/livingstonb/GitHub/Continuous_Time_HA/';
 runopts.localdir = '/Users/Brian-laptop/Documents/GitHub/Continuous_Time_HA/';
