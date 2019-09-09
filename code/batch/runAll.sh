feab7b5063d70723ba4a7a3ae770497ac9a60f0e
Brian Livingston <ak.brian09@gmail.com>
2019-09-09 11:00:23 -0500

    fix to bash script

--- code/batch/runAll.sh
+++ code/batch/runAll.sh
@@ -1,5 +1,5 @@
 rm /home/livingstonb/GitHub/Continuous_Time_HA/output/two_asset/*
 rm /home/livingstonb/GitHub/Continuous_Time_HA/output/con_effort/*
 
-sbatch /home/livingstonb/GitHub/Continuous_Time_HA/code/two_asset/table_tests.sbatch
-sbatch /home/livingstonb/GitHub/Continuous_Time_HA/code/con_effort/server.sbatch
+sbatch /home/livingstonb/GitHub/Continuous_Time_HA/code/batch/two_asset/table_tests.sbatch
+sbatch /home/livingstonb/GitHub/Continuous_Time_HA/code/batch/con_effort/server.sbatch
